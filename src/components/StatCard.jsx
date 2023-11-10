b341884ecc4b992c95453f667785b2aa00656aae
soni <acdsoni@gmail.com>
2023-11-10 15:47:57 +0100

    General Stats

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import starIcon from "../assets/calendar-star.svg";
+import { ArrowDownward,  ArrowUpward} from "@mui/icons-material";
 
-export default function StatCard({ title = "Number of Orders" }) {
+export default function StatCard({
+  title = "Number of Orders",
+  value = 208,
+  percentGain = -2,
+}) {
   return (
     <div
       style={{
@@ -12,7 +17,14 @@ export default function StatCard({ title = "Number of Orders" }) {
         backgroundColor: "white",
       }}
     >
-      <div style={{ display: "flex", gap: "8px", alignItems: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          gap: "8px",
+          padding: "8px",
+          alignItems: "center",
+        }}
+      >
         <div
           style={{
             height: "36px",
@@ -33,12 +45,60 @@ export default function StatCard({ title = "Number of Orders" }) {
         <p
           style={{
             fontSize: "14px",
+            margin: 0,
             fontWeight: 700,
             fontFamily: "LibreFranklin",
           }}
         >
           {title}
         </p>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          padding: "8px",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <p
+          style={{
+            fontSize: "20px",
+            margin: 0,
+            fontWeight: 700,
+            fontFamily: "LibreFranklin",
+          }}
+        >
+          {value}
+        </p>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            backgroundColor: percentGain >= 0 ? "#DFFEF5" : "#FFE1E6",
+            padding: "4px 8px",
+            borderRadius: "8px",
+          }}
+        >
+          <p
+            style={{
+              fontSize: "15px",
+              margin: 0,
+              fontWeight: 700,
+              fontFamily: "LibreFranklin",
+              color: percentGain >= 0 ? "#0FAE7F" : "#761727",
+            }}
+          >
+            {`${Math.abs(percentGain)}%`}
+          </p>
+          {percentGain >= 0 ? (
+            <ArrowUpward fontSize="small" style={{ color: "#0FAE7F" }} />
+          ) : (
+            <ArrowDownward fontSize="small" style={{ color: "#761727" }} />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import StatCard from '../components/StatCard'
+import React from "react";
+import StatCard from "../components/StatCard";
+
+const monthlySalesStats = [
+  { title: "Number of Orders", value: 208, percentGain: -2 },
+  { title: "AVG Review Score", value: 6, percentGain: -2 },
+  { title: "Cancellations", value: 54, percentGain: 2 },
+  { title: "Done", value: 72, percentGain: 2 },
+  { title: "Customers", value: 12, percentGain: 2 },
+];
 
 export default function Dashboard() {
   return (
-    <div>
-       <StatCard/>
-    </div>
-  )
+    <>
+      <div style={{display: 'flex', padding: '8px', gap: '8px', flexWrap: 'wrap'}}>
+        {monthlySalesStats.map((item, index) => (
+          <StatCard
+            key={index}
+            title={item.title}
+            value={item.value}
+            percentGain={item.percentGain}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
